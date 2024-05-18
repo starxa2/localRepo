7c21feb576b0b20ff8edcb112c133b905b9b2deb
starxa2 <shinde.abhishek222001@gmail.com>
2024-05-18 21:42:15 +0530

    Updated pythonfile

--- tmpLocal.py
+++ tmpLocal.py
@@ -1,1 +1,17 @@
 # Normal python file
+
+# git remote add origin <-link->
+# This will make the current localRepo "origin" and upload it on the given link
+
+to check which is the origin use "git remote -v"
+"git remote -v" is used to check what is the current git repo
+
+to check the current branch 
+"git branch"
+
+to change the name of the current branch use
+"git branch -M main"
+
+"git push -u origin main"
+after using this from the next time you only need to write
+"git push"
